4385f42e348c0d3ab0f8271a8ab1ed50f4155bd9
= <redman29h@gmail.com>
2025-08-04 01:54:46 +1000

    calendar improvements

--- src/server/api/service/calendar.ts
+++ src/server/api/service/calendar.ts
@@ -1,7 +1,5 @@
-import { calendar_v3, google } from "googleapis";
+import { type calendar_v3, google } from "googleapis";
 import { ExcuseApi } from "./excuseapi";
-import { calendar } from "googleapis/build/src/apis/calendar";
-import { readFileSync } from "fs";
 
 function getCalendarClient(access_token: string | null) {
   if (!access_token) {
@@ -14,6 +12,78 @@ function getCalendarClient(access_token: string | null) {
   auth.setCredentials({ access_token: access_token });
   google.options({ http2: true });
   return google.calendar({ version: "v3", auth });
+}
+
+function distanceToEvent(event: calendar_v3.Schema$Event, time_ms: number) {
+  return Math.min(
+    Math.abs(new Date(event.start?.dateTime ?? 0).getTime() - time_ms),
+    Math.abs(new Date(event.end?.dateTime ?? 0).getTime() - time_ms),
+  );
+}
+
+function timeDifference(a: Date, b: Date): string {
+  const diff = Math.abs(a.getTime() - b.getTime());
+  const mins = Math.floor(diff / 1000 / 60);
+  const hours = Math.floor(mins / 60);
+  const days = Math.floor(hours / 24);
+  if (days > 0) {
+    return `${days} days`;
+  }
+  if (hours > 0) {
+    return `${hours} hours`;
+  }
+  return `${mins} minutes`;
+}
+
+function determineTimeRelation(event: calendar_v3.Schema$Event) {
+  const now = Date.now();
+  if (!event.start && !event.end) {
+    return "Unknown";
+  }
+
+  if (event.end?.dateTime && new Date(event.end.dateTime).getTime() < now) {
+    return `Finished ${timeDifference(
+      new Date(event.end.dateTime),
+      new Date(),
+    )} ago`;
+  }
+
+  if (event.start?.dateTime && new Date(event.start.dateTime).getTime() > now) {
+    return `Starts in ${timeDifference(
+      new Date(event.start.dateTime),
+      new Date(),
+    )}`;
+  }
+
+  if (
+    event.start?.dateTime &&
+    event.end?.dateTime &&
+    new Date(event.end.dateTime).getTime() < now &&
+    new Date(event.start.dateTime).getTime() > now
+  ) {
+    return "Ongoing";
+  }
+  return "Unknown";
+}
+
+function calendarDateTimeToString(
+  calendarDateTime: calendar_v3.Schema$EventDateTime | undefined,
+) {
+  if (!calendarDateTime) {
+    return "Unknown Time";
+  }
+  if (calendarDateTime.date) {
+    return calendarDateTime.date;
+  }
+  if (calendarDateTime.dateTime) {
+    return calendarDateTime.dateTime;
+  }
+  return "Unknown Time";
+}
+
+function convertEventToString(event: calendar_v3.Schema$Event) {
+  return `${event.summary}${event.start?.date ? `${event.start?.date} all day` : `from ${calendarDateTimeToString(event.start)} to ${calendarDateTimeToString(event.end)}`} - ${determineTimeRelation(event)}
+                ${event.description ? `Description ${event.description}` : ""}`;
 }
 
 class CalendarApi extends ExcuseApi {
@@ -43,18 +113,8 @@ class CalendarApi extends ExcuseApi {
 
       const events = res.data.items ?? [];
 
-      function distanceToEvent(
-        event: calendar_v3.Schema$Event,
-        time_ms: number,
-      ) {
-        return Math.min(
-          Math.abs(new Date(event.start?.dateTime ?? 0).getTime() - time_ms),
-          Math.abs(new Date(event.end?.dateTime ?? 0).getTime() - time_ms),
-        );
-      }
-
       const now = Date.now();
-      // Fix events issues
+      // Fix all day event issues
       events.forEach((event) => {
         if (event.start && !event.start.dateTime && event.start.date) {
           event.start.dateTime = new Date(event.start.date).toISOString();
@@ -69,60 +129,6 @@ class CalendarApi extends ExcuseApi {
         return distanceToEvent(a, now) - distanceToEvent(b, now);
       });
 
-      function timeDifference(a: Date, b: Date): string {
-        const diff = Math.abs(a.getTime() - b.getTime());
-        const mins = Math.floor(diff / 1000 / 60);
-        const hours = Math.floor(mins / 60);
-        const days = Math.floor(hours / 24);
-        if (days > 0) {
-          return `${days} days`;
-        }
-        if (hours > 0) {
-          return `${hours} hours`;
-        }
-        return `${mins} minutes`;
-      }
-
-      function determineTimeRelation(event: calendar_v3.Schema$Event) {
-        if (!event.start && !event.end) {
-          return "Unknown";
-        }
-
-        if (
-          event.end?.dateTime &&
-          new Date(event.end.dateTime).getTime() < now
-        ) {
-          return `Finished ${timeDifference(
-            new Date(event.end.dateTime),
-            new Date(),
-          )} ago`;
-        }
-
-        if (
-          event.start?.dateTime &&
-          new Date(event.start.dateTime).getTime() > now
-        ) {
-          return `Starts in ${timeDifference(
-            new Date(event.start.dateTime),
-            new Date(),
-          )}`;
-        }
-
-        if (
-          event.start?.dateTime &&
-          event.end?.dateTime &&
-          new Date(event.end.dateTime).getTime() < now &&
-          new Date(event.start.dateTime).getTime() > now
-        ) {
-          return "Ongoing";
-        }
-        return "Unknown";
-      }
-
-      function convertEventToString(event: calendar_v3.Schema$Event) {
-        return `${event.summary} from ${event.start ? event.start.dateTime : "Unknown Start Time"} to ${event.end ? event.end.dateTime : "Unknown End Time"} - ${determineTimeRelation(event)}
-                ${event.description ? `Description ${event.description}` : ""}`;
-      }
       return events.reduce((acc, event) => {
         return acc + "   " + convertEventToString(event) + "\n";
       }, `Recent Events Information from ${calendar_description} (Closest events first)\n`);
